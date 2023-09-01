82f22db73c1a3d555e936c8db5e89d0fd24316f7
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-09-01 13:18:31 +0100

    info: message format file

---
+++ utils/messages.js
@@ -0,0 +1,11 @@
+// message format
+const moment = require("moment");
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("hh:mm a"),
+  };
+}
+
+module.exports = { formatMessage };
